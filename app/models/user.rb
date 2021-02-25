0664a63a7840b56c5f9a5a13a3247a4b6a8acd2a
shin1616 <s.murata0416@gmail.com>
2021-02-25 19:01:36 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  VALID_PASSWORD_MIX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_NAME_KANJI = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  VALID_NAME_KANA = /\A[ァ-ヶ]+\z/
+  validates :password,         presence: true, format: { with: VALID_PASSWORD_MIX }
   validates :nickname,         presence: true
-  validates :family_name,      presence: true
-  validates :first_name,       presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana,  presence: true
+  validates :family_name,      presence: true, format: { with: VALID_NAME_KANJI }
+  validates :first_name,       presence: true, format: { with: VALID_NAME_KANJI }
+  validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA }
+  validates :first_name_kana,  presence: true, format: { with: VALID_NAME_KANA }
   validates :birth_day,        presence: true
 end
