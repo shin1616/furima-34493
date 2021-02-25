8e72fb4e00109829f79cc83a11bf5ca73b0415c2
shin1616 <s.murata0416@gmail.com>
2021-02-25 19:01:18 +0900

    FactoryBotの作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
+    password_confirmation {password}
+    family_name           {'山田'}
+    first_name            {'太郎'}
+    family_name_kana      {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+    birth_day             {Faker::Date.between(from: '1930-01-01', to: 3.year.ago)}
+  end
+end
