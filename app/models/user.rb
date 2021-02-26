c11e52f99faf9207bb5ea7e06fe78e1ddd44a9fc
shin1616 <s.murata0416@gmail.com>
2021-02-26 15:18:08 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
   VALID_PASSWORD_MIX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   VALID_NAME_KANJI = /\A[ぁ-んァ-ヶ一-龥々]+\z/
   VALID_NAME_KANA = /\A[ァ-ヶ]+\z/
-  validates :password,         presence: true, format: { with: VALID_PASSWORD_MIX }
-  validates :nickname,         presence: true
-  validates :family_name,      presence: true, format: { with: VALID_NAME_KANJI }
-  validates :first_name,       presence: true, format: { with: VALID_NAME_KANJI }
-  validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA }
-  validates :first_name_kana,  presence: true, format: { with: VALID_NAME_KANA }
-  validates :birth_day,        presence: true
+
+  with_options presence: true do
+    validates :password,          format: { with: VALID_PASSWORD_MIX }
+    validates :nickname
+    validates :family_name,       format: { with: VALID_NAME_KANJI }
+    validates :first_name,        format: { with: VALID_NAME_KANJI }
+    validates :family_name_kana,  format: { with: VALID_NAME_KANA }
+    validates :first_name_kana,   format: { with: VALID_NAME_KANA }
+    validates :birth_day
+  end
 end
