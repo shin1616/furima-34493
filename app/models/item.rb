67ea78ce47aa16356977d250adc397259997c8e7
shin1616 <s.murata0416@gmail.com>
2021-02-26 18:12:21 +0900

    itemモデルとマイグレーションファイルを作成

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name          
+    validates :price         
+    validates :text          
+    validates :category_id   
+    validates :status_id     
+    validates :postage_id    
+    validates :prefecture_id 
+    validates :period_id     
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_045608) do
+ActiveRecord::Schema.define(version: 2021_02_26_083702) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "text", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "period_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
