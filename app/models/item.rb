c9b5ac5d2fed6372c2e77522c3e595c410505e90
shin1616 <s.murata0416@gmail.com>
2021-02-27 21:46:42 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,15 +6,25 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :period
+  has_one_attached :image
 
+  VALID_PRICE = /\A[0-9]+\z/
   with_options presence: true do
-    validates :name          
-    validates :price         
-    validates :text          
-    validates :category_id   
-    validates :status_id     
-    validates :postage_id    
-    validates :prefecture_id 
-    validates :period_id     
+    validates :name
+    validates :price,format: { with: VALID_PRICE }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 999999}
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :period_id
+    validates :image
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :period_id
   end
 end
