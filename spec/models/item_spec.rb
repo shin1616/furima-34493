0c13aefe5fd61c7171f2050fad8fcc3e831c2615
shin1616 <s.murata0416@gmail.com>
2021-03-01 15:53:51 +0900

    integer型の記述を'数字'から数字のみに修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it '金額が300円以上で9,999,999円以下かつ、半角数字だと登録できる' do
-        @item.price = '5000'
+        @item.price = 5000
         expect(@item).to be_valid
       end
     end
@@ -73,12 +73,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it '販売価格は300円以上でないと登録できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '販売価格は9,999,999円以下でないと登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
