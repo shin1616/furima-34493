a1860519dd4c04482407d1d2c452315fcdbc3f65
shin1616 <s.murata0416@gmail.com>
2021-03-01 15:54:11 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,25 +37,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーの情報がid:1では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
       it '商品の状態が空では登録できない' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '商品の状態がid:1では登録できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it '配送料の負担が空では登録できない' do
         @item.postage_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Postage can't be blank")
       end
+      it '配送料の負担がid:1では登録できない' do
+        @item.postage_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage must be other than 1")
+      end
       it '発送元の地域が空では登録できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
+      it '発送元の地域がid:1では登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
       it '発送までの日数が空では登録できない' do
         @item.period_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Period can't be blank")
+      end
+      it '発送までの日数がid:1では登録できない' do
+        @item.period_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Period must be other than 1")
       end
       it '販売価格が空では登録できない' do
         @item.price = ''
