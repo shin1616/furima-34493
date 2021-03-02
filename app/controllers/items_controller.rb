8123b790864b5c6958f2b2295b33117ab93ee252
shin1616 <s.murata0416@gmail.com>
2021-03-02 12:48:38 +0900

    ユーザーによる制限を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items = Item.order('created_at DESC')
@@ -40,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :text, :image, :category_id, :status_id, :postage_id, :prefecture_id,
                                  :period_id).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
