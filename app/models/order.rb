d8f86bf5b28c028206a9d0090391e868b24549ca
shin1616 <s.murata0416@gmail.com>
2021-03-04 19:19:54 +0900

    アソシエーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
-class Orders < ApplicationRecord
+class Order < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :token
   validates :price, presence: true
+  validates :token, presence: true
+
+  belongs_to :user
+  has_one :deliveries
 end
