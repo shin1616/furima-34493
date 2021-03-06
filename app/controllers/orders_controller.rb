e5f4f73996cbf58f1f4ff642516c673bd7b802b3
shin1616 <s.murata0416@gmail.com>
2021-03-06 18:18:13 +0900

    orderコントローラーの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
+
   def index
-    @order = Order.new
+    @order_delivery = OrderDelivery.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
       pay_item
-      @order.save
-      return redirect_to root_path
+      @order_delivery.save
+      redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
 
-  def order_params
-    params.require(:order).permit(:price).merge(token: params[:token])
+  def set_order
+    @item = Item.find(params[:item_id])
   end
 
-  def pay_item
+  def order_params
+    params.require(:order_delivery).permit(:post_number, :prefecture_id, :municipality, :address, :apartment, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item 
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
-      card: order_params[:token],
-      currency: 'jpy'
+    amount: @item.price,
+    card: order_params[:token],
+    currency: 'jpy'
     )
   end
 
