9e5258ecdf89304f0e31a07713f7eec5c62eaaae
shin1616 <s.murata0416@gmail.com>
2021-03-06 18:19:35 +0900

    モデルの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,5 @@
 class Order < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :token
-  validates :price, presence: true
-  validates :token, presence: true
-
   belongs_to :user
-  has_one :deliveries
+  belongs_to :item
+  has_one :delivery
 end
