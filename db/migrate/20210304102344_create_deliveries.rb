eac6f2bef4c907000ff2a41182c64dcaeb59bc03
shin1616 <s.murata0416@gmail.com>
2021-03-08 14:42:54 +0900

    マイグレーションファイルの修正

--- db/migrate/20210304102344_create_deliveries.rb
+++ db/migrate/20210304102344_create_deliveries.rb
@@ -5,7 +5,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
       t.integer    :prefecture_id, null: false
       t.string     :municipality,  null: false
       t.string     :address,       null: false
-      t.string     :apartment,     null: false
+      t.string     :apartment
       t.string     :phone_number,  null: false
       t.references :order,         null: false, foreign_key: true
       t.timestamps
