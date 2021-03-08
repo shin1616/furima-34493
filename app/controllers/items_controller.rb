652e1c67784df5ab16159c427a4722b4f0de5f8b
shin1616 <s.murata0416@gmail.com>
2021-03-08 15:14:27 +0900

    Itemコントローラーの可読性を向上させるため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, expect: [:index, :show]
   before_action :set_item,             only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index,      except: [:index, :show, :new, :create]
+  before_action :move_to_index,        only: [:edit, :update, :destroy]
   before_action :done_order,           only: [:edit, :update, :destroy]
 
   def index
