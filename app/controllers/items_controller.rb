5a0d1bc557634b9a1f57a91bb51e9d79b16861aa
shin1616 <s.murata0416@gmail.com>
2021-03-08 14:53:06 +0900

    itemコントローラーの制限の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item,           only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
-  before_action :done_order, only: [:edit, :update]
+  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :set_item,             only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index,      except: [:index, :show, :new, :create]
+  before_action :done_order,           only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
