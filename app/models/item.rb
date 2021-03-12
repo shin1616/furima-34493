6d6c6b97448aadc87acb71bc4fcd9e33a2f67c16
shin1616 <s.murata0416@gmail.com>
2021-03-13 00:35:02 +0900

    エラー文の日本語化の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,7 @@ class Item < ApplicationRecord
     validates :period_id
     validates :images
   end
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: "を選択してください" } do
     validates :category_id
     validates :status_id
     validates :postage_id
